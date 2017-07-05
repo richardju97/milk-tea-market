540c7021a64177a1b5c0a0eb61c8605cfd7dbe1e
rju <rju@purdue.edu>
2017-07-05 22:54:47 +0800

    Added preferences, currently stored as 1 random int

--- customer.py
+++ customer.py
@@ -1,4 +1,15 @@
 # customer.py
 # Contains everything to do with the customer object
 
+from random import randint
 
+class Customer:
+    def __init__(self):
+        self.preferences = randint(0, 100)
+
+    def getPreferences(self):
+        return self.preferences
+
+# Test
+A = Customer()
+print(A.getPreferences())
