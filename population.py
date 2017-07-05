849d123a498bbd3002d4d4f3267b967946d752ed
rju <rju@purdue.edu>
2017-07-05 23:23:45 +0800

    Created population array

--- population.py
+++ population.py
@@ -1,2 +1,16 @@
 # population.py
 # Contains the entire population (customers) of your market
+
+import customer
+
+marketSize = 5
+
+#A = customer.Customer()
+#print(A.getPreferences())
+
+population = []
+
+for i in range(0, marketSize):
+    population.append(customer.Customer().getPreferences())
+
+print(population)
