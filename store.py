13cc31962e42cf56d31fc6658b21a19f0dfc8afd
rju <rju@purdue.edu>
2017-07-06 11:23:31 +0800

    Added daily revenue and total revenue attributes

--- store.py
+++ store.py
@@ -5,6 +5,8 @@ class Store:
     def __init__(self, name, price):
         self.name = name
         self.price = price
+        self.totalrevenue = 0
+        self.dailyrevenue = 0
 
     def changePrice(self, price):
         self.price = price
