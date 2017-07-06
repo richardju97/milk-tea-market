c1fed0300b2ebccbafa4dc7bde0a340681415643
rju <rju@purdue.edu>
2017-07-06 11:11:41 +0800

    Added init and changePrice function

--- store.py
+++ store.py
@@ -1,2 +1,10 @@
 # store.py
 # Handles store objects for each player
+
+class Store:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+    def changePrice(self, price):
+        self.price = price
