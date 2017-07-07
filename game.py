30f3f59f18fd0b8e309622a479053bd0888cb671
rju <rju@purdue.edu>
2017-07-07 23:42:39 +0800

    Typos/bugs; sales is not correctly displayed

--- game.py
+++ game.py
@@ -7,7 +7,7 @@ import store
 
 numPlayers = int(input("Enter Number of Players Playing: "))
 populationSize = int(input("Enter desired population size for this simulation: "))
-maxDays = int(input("Enter maximum number of operation days:"))
+maxDays = int(input("Enter maximum number of operation days: "))
 allPlayers = []
 
 for x in range(0, numPlayers):
@@ -30,7 +30,7 @@ print("----------------------\n")
 day = 0
 while (day != maxDays):
 #while (allPlayers[0].getPrice() < 11 and sales != 0):
-    sales = 0
+#    sales = 0
     for i in p:
         if (i >= allPlayers[0].getPrice()):
             # for now we assume that all places have the same price
@@ -40,7 +40,7 @@ while (day != maxDays):
 
 
     for x in range(0, numPlayers):
-        print("We had " + str(sales) + " sales at $" + str(allPlayers[x].getPrice()) + " per sale.")
+        print(str(allPlayers[x].getName()) + " had " + str(allPlayers[x].getDailySales()) + " sales at $" + str(allPlayers[x].getPrice()) + " per sale.")
         print("We made $" + str(allPlayers[x].getDailyRevenue()) + " today.")
         print("----------------------\n")
         allPlayers[x].changePrice(allPlayers[x].getPrice() + 1)
