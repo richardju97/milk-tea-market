0c68a822066a4b2fe2d830f344b8c6b76ecc5b18
rju <rju@purdue.edu>
2017-07-10 16:42:29 +0800

    Added x-axis label

--- stats.py
+++ stats.py
@@ -7,8 +7,9 @@ def generateGraph(data1, label1, data2, label2):
     plt.subplot(2, 1, 1)
     plt.plot(data1)
     plt.ylabel(label1)
+    plt.xlabel("Days of Operation")
     plt.subplot(2, 1, 2)
     plt.plot(data2)
     plt.ylabel(label2)
-    
+    plt.xlabel("Days of Operation")
     plt.show()
