e817c41076eb1b8a209d179ab0ba5f907c0a13fc
rju <rju@purdue.edu>
2017-07-10 21:31:20 +0800

    Added object w/ adjustPrice function

--- rawmaterials.py
+++ rawmaterials.py
@@ -1,4 +1,10 @@
 # rawmaterials.py
 # All data pertaining to raw materials that each cafe can purchase
 
+class RawMaterial:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
 
+    def adjustPrice(self, delta):
+        self.price += delta
