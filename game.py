6bb5a217903fd437fd7b89399346e4dccf15412f
rju <rju@purdue.edu>
2017-07-13 00:04:17 +0800

    Cleaned up comments

--- game.py
+++ game.py
@@ -10,18 +10,8 @@ print("Welcome to Milk Tea Mania! Please configure your settings:")
 numPlayers = int(input("Enter Number of Players Playing: "))
 populationSize = int(input("Enter desired population size for this simulation: "))
 maxDays = int(input("Enter maximum number of operation days: "))
-#allPlayers = []
 
 print("----------------------\n")
-
-#for x in range(0, numPlayers):
-#
-#    myStoreName = input("Please select your store's name: ")
-#    myStorePrice = input("Please select what price to sell your boba: ")
-#    print("----------------------\n")
-#    allPlayers.append(store.Store(myStoreName, myStorePrice))
-#    #myStore = store.Store(myStoreName, myStorePrice)
-#    #print(myStore)
 
 allPlayers = market.createMarket(numPlayers)
 
@@ -31,29 +21,22 @@ print("There are " + str(len(p)) + " potential customers.")
 print("----------------------\n")
 
 # At the moment preferences only contains maximum value point (how much the customer is willing to pay at most)
-#sales = None
-#days = 0
 day = 0
 while (day != maxDays):
 #while (allPlayers[0].getPrice() < 11 and sales != 0):
-#    sales = 0
     for i in p:
         if (i >= allPlayers[0].getPrice()):
             # for now we assume that all places have the same price
             decision = randint(0, numPlayers-1)
             allPlayers[decision].processSale()
             allPlayers[decision].addRating(randint(1, 5))
-#            sales += 1
 
     print("Day " + str(day+1))
     for x in range(0, numPlayers):
         print(str(allPlayers[x].getName()) + " - Sales ($" + str(allPlayers[x].getPrice()) + "):x $" + str(allPlayers[x].getDailySales())  + " - Revenue: $" + str(allPlayers[x].getDailyRevenue()) + " - Rating: " + str(allPlayers[x].getRating()))
-#        print("We made $" + str(allPlayers[x].getDailyRevenue()) + " today.")
-#        print("----------------------\n")
         allPlayers[x].changePrice(allPlayers[x].getPrice() + 1)
         if (allPlayers[x].getDailyRevenue() != 0):
             allPlayers[x].aggregateRevenue()
-#        days += 1
     day += 1
     print("----------------------\n")
 
